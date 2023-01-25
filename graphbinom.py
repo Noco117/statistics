dd7eaadfb851c9b812977ddc0793fd7f5907f66c
Noco117 <nicolai.prodanov@gmail.com>
2023-01-25 23:56:51 +0100

    first commit

---
+++ graphbinom.py
@@ -0,0 +1,12 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.stats import binom
+
+def graph_binomial_distribution(n, p, c_level):
+
+    # Create k
+    k = np.arange(0, n+1)
+
+    # creat plot
+
+
